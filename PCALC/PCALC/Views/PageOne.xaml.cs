7118620e05edc2833d0fc654f9a6594034397484
Edgardo-R <edgardo291196@gmail.com>
2022-04-06 20:26:25 -0600

    Agregar archivos de proyecto.

---
+++ PCALC/PCALC/Views/PageOne.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PCALC.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageOne : ContentPage
+    {
+        public PageOne()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSuma_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Resultado: ", Convert.ToString(Convert.ToDouble(num1.Text) + Convert.ToDouble(num2.Text)), "Aceptar");
+        }
+
+        private async void btnResta_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Resultado: ", Convert.ToString(Convert.ToDouble(num1.Text) - Convert.ToDouble(num2.Text)), "Aceptar");
+        }
+
+        private async void btnMult_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Resultado: ", Convert.ToString(Convert.ToDouble(num1.Text) * Convert.ToDouble(num2.Text)), "Aceptar");
+        }
+
+        private async void btnDiv_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Resultado: ", Convert.ToString(Convert.ToDouble(num1.Text) / Convert.ToDouble(num2.Text)), "Aceptar");
+        }
+    }
+}
